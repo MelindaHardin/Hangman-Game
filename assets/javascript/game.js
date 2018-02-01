4fc580cdbed61a61391c82328fcd7f0ce5944d95
Melinda Hardin <melindawad@gmail.com>
2018-01-31 22:52:54 -0600

    jan 31

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+document.onkeypress = function (start) {
+    start =
+}
+    
+    var roundWins = 0;
+    var losses = 0;
+    var guessesRemaining = 0;
+    var correct = 0;
+    var userGuess = 0;
+
+
+
+            
