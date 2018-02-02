aaa6b4738c2c4776555e01330b647588a080ce3d
Melinda Hardin <melindawad@gmail.com>
2018-02-01 18:23:38 -0600

    Feb 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,28 @@
-document.onkeypress = function (start) {
-    start =
-}
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 5;
+var wrongGuesses = 0;
+
+
+
+
+var electricSlide = ["e", "l","e", "c", "t", "r", "i", "c", "s", "l", "i", "d", "e"];
+
+var macarina ="macarina";
+
+
+document.onkeyup=function (event) {
+    var letterGuessed = event.key.toLowerCase();
+
+
+
+    if (macarina.indexOf(letterGuessed) !== -1){
+        console.log("letter Matched");
+    }
     
-    var roundWins = 0;
-    var losses = 0;
-    var guessesRemaining = 0;
-    var correct = 0;
-    var userGuess = 0;
-
-
-
-            
+    else {
+        guessesLeft--;
+        console.log ("guesses left =" + guessesLeft)
+    }
+}
