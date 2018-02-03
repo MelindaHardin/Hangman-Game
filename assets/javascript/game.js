8f034ecbd86cfb50a590c4d3f6691749487df186
Melinda Hardin <melindawad@gmail.com>
2018-02-02 20:19:52 -0600

    Feb 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,35 @@
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
+var guessesLeft = 20;
 var wrongGuesses = 0;
 
 
 
 
-var electricSlide = ["e", "l","e", "c", "t", "r", "i", "c", "s", "l", "i", "d", "e"];
+var macarena ="macarena";
+var word1 = macarena.toUpperCase().split(""); 
 
-var macarina ="macarina";
+var electricSlide = "electric slide";
+var word2 = electricSlide.toUpperCase().split(""); 
 
+
+console.log ("word: ", word1);
+
+
+for (i=0; i<word1.length; i++){
+    document.getElementById("guessedCorrect" +word1[i]);
+}
 
 document.onkeyup=function (event) {
-    var letterGuessed = event.key.toLowerCase();
+    var letterGuessed = event.key.toUpperCase();
 
 
 
-    if (macarina.indexOf(letterGuessed) !== -1){
+    if (macarena.indexOf(letterGuessed) !== -1){
         console.log("letter Matched");
+        var index= macarena.indexOf(letterGuessed);
+        macarena[index]=letterGuessed;
     }
     
     else {
